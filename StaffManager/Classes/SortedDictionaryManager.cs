4c932d8e277c04b23ef7dd3009c90afcc1146257
Michael McKIE <michael_mckie@hotmail.com>
2025-09-21 18:56:37 +0800

    Minor code refractor to improve useability

--- StaffManager/Classes/SortedDictionaryManager.cs
+++ StaffManager/Classes/SortedDictionaryManager.cs
@@ -3,37 +3,39 @@ using CommunityToolkit.Mvvm.Input;
 using ErrorLogging;
 using StaffManager.DataModels;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
 
 namespace StaffManager.Classes;
 
 public partial class SortedDictionaryManager: ObservableObject {
+    public StaffData StaffData { get; } = new StaffData();
     public event Action? RequestNewWindow;
     public event Action? RequestClose;
     public event Action? RequestApplicationClose;
 
-    [ObservableProperty] private SortedDictionary<int, string> _masterFile = [];
-    [ObservableProperty] private ObservableCollection<StaffMember> _staffMembers = [];
-    [ObservableProperty] private string? _searchTerm = string.Empty;
-    [ObservableProperty] private string? _searchStaffName = string.Empty;
-    [ObservableProperty] private int _searchStaffId;
-    [ObservableProperty] private StaffMember _selectedStaffMemeber = new();
-    [ObservableProperty] private bool _shouldFocusIdTextBox;
-    [ObservableProperty] private bool _shouldFocusNameTextBox;
-    [ObservableProperty] private string? _selectedStaffName = "";
-    [ObservableProperty] private int ? _selectedStaffId;
-
     public SortedDictionaryManager (){
-        DataManager.InitialiseData(_masterFile);
+        if (StaffData.MasterFile.SortedData != null){
+            DataManager.InitialiseData(StaffData.MasterFile.SortedData);
+        }
+
+        StaffData.PropertyChanged += StaffDataPropertyChanged;
     }
 
-    partial void OnSearchTermChanged (string? value){
-        FilterStaffMembers(value);
+    private void StaffDataPropertyChanged (object? sender, PropertyChangedEventArgs e){
+        if (e.PropertyName == nameof(StaffData.SearchTerm)){
+            FilterStaffMembers(StaffData.SearchTerm);
+        }
+
+        if (e.PropertyName == nameof(StaffData.SelectedStaffMember)){
+            OnSelectedStaffMemberChanged(StaffData.SelectedStaffMember);
+        }
     }
 
-    partial void OnSelectedStaffMemeberChanged (StaffMember value){
+    private void OnSelectedStaffMemberChanged (StaffMember? value){
         if (value != null){
-            SelectedStaffName = value.Name;
-            SelectedStaffId = value.Id;
+            StaffData.SelectedStaffName = value.Name;
+            StaffData.SelectedStaffId = value.Id;
         }
     }
 
@@ -42,31 +44,45 @@ public partial class SortedDictionaryManager: ObservableObject {
             searchTerm = searchTerm?.Trim() ?? "";
             IEnumerable<StaffMember> filtered;
 
+            if (StaffData.MasterFile.SortedData == null){
+                LoggingManager.Instance.LogWarning("Failed to filter the staff data no data was found.");
+                return;
+            }
+
             if (string.IsNullOrWhiteSpace(searchTerm)){
                 filtered = [];
             } else if (int.TryParse(searchTerm, out _)){
-                filtered = MasterFile.Where(kvp => kvp.Key.ToString().StartsWith(searchTerm)).Select(kvp => new StaffMember { Id = kvp.Key, Name = kvp.Value });
+                filtered = StaffData.MasterFile.SortedData.Where(kvp => kvp.Key.ToString().StartsWith(searchTerm)).Select(kvp => new StaffMember { Id = kvp.Key, Name = kvp.Value });
             } else {
-                filtered = MasterFile.Where(kvp => kvp.Value.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).Select(kvp => new StaffMember { Id = kvp.Key, Name = kvp.Value });
+                filtered = StaffData.MasterFile.SortedData.Where(kvp => kvp.Value.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).Select(kvp => new StaffMember { Id = kvp.Key, Name = kvp.Value });
             }
 
-            StaffMembers = new ObservableCollection<StaffMember>(filtered);
+            StaffData.StaffMemebers = new ObservableCollection<StaffMember>(filtered);
         } catch (Exception ex){
             LoggingManager.Instance.LogError(ex, "Failed to Filter the staff data.");
         }
     }
 
     [RelayCommand] private void ClearId (){
-        if (SelectedStaffMemeber != null){
-            SelectedStaffId = null;
-            ShouldFocusIdTextBox = true;
-        }
+        ClearSelectedField(false);
     }
 
     [RelayCommand] private void ClearName (){
-        if (SelectedStaffMemeber != null){
-            SelectedStaffName = string.Empty;
-            ShouldFocusNameTextBox = true;
+        ClearSelectedField(true);
+    }
+
+    private void ClearSelectedField (bool clearName){
+        if (StaffData.SelectedStaffMember != null){
+            if (clearName){
+                StaffData.SelectedStaffName = string.Empty;
+                StaffData.ShouldFocusNameTextBox = true;
+            } else {
+                StaffData.SelectedStaffId = null;
+                StaffData.ShouldFocusIdTextBox = true;
+            }
+        } else {
+            LoggingManager.Instance.LogWarning("Failed to clear the selection field, no selected staff member was found.");
+            MessageBox.Show("", "No Data Found Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
     }
 
